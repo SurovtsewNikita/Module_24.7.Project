1f14f44bc95a1711aab1bfc4c592080decae9caa
Harlos <surovtzew.nikita@yandex.ru>
2023-04-03 19:04:28 +0300

    Задание 24.7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ import models.University;
 
 public class Main {
     public static void main(String[] args) {
-        University university = new University("1134-7514","Higher School of Economics","HSE",1992, StudyProfile.JAVA);
-        Student student = new Student("Maks","1GT5-4YS8",3,(float)(3.75));
+        University university = new University("1134-7514", "Higher School of Economics", "HSE", 1992, StudyProfile.JAVA);
+        Student student = new Student("Maks", "1GT5-4YS8", 3, (float) (3.75));
         System.out.println(university);
         System.out.println(student);
 
